b3912747d688fa5d8934126b3841b79ce152cb9c
Dawidemm <dawid.mazur@icloud.com>
2023-12-04 10:54:13 +0100

    Add train, val, test step in LitModel

--- project/Model.py
+++ project/Model.py
@@ -21,18 +21,28 @@ class myLitModel(pl.LightningModule):
     def __init__(self, model):
         super().__init__()
         self.model = model
+        self.criterion = nn.MSELoss()
 
     def forward(self, x):
         return self.model(x)
     
     def training_step(self, batch, batch_idx):
-        pass
+        x, y = batch
+        outputs = self.model(x)
+        loss = self.criterion(outputs, y)
+        return loss
 
     def validation_step(self, batch, batch_idx):
-        pass
+        x, y = batch
+        outputs = self.model(x)
+        loss = self.criterion(outputs, y)
+        return {"val_loss": loss}
 
     def test_step(self, batch, batch_idx):
-        pass
+        x, y = batch
+        outputs = self.model(x)
+        loss = self.criterion(outputs, y)
+        return {"test_loss": loss}
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=0.01)
@@ -54,4 +64,5 @@ torch_model = myModel()
 lit_model = myLitModel(torch_model)
 
 trainer = pl.Trainer(max_epochs=10)
-trainer.fit(lit_model, train_dataloaders=train_dataloader)
+trainer.fit(lit_model, train_dataloaders=train_dataloader, val_dataloaders=val_dataloader)
+trainer.test(dataloaders=test_dataloader)
