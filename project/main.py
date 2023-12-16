75020b53cf232b63a6c327676d8c7f9008775ecc
Dawidemm <dawid.mazur@icloud.com>
2023-12-16 21:38:42 +0100

    Update main

--- project/main.py
+++ project/main.py
@@ -1,17 +1,12 @@
-import cv2
-import numpy as np
-import pandas as pd
 import torch
-from torch.utils.data import DataLoader
-from mydataset import myDataset
+import numpy as np
 from fetchdata import FetchData
-from model import myModel, myLitModel
+from model import DataStep, myModel, myLitModel
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.animation import FuncAnimation
-from torchmetrics import R2Score
-from sklearn.metrics import r2_score
+from torchmetrics import MeanAbsoluteError
 
+DATA_STEP = DataStep.step
 EARTH_RADIUS = 6371
 ISS_ORBIT_RADIUS = 420
 animation_paused = False
@@ -31,48 +26,76 @@ def draw_earth(ax):
     z = EARTH_RADIUS * np.cos(theta)
     ax.plot_surface(x, y, z, color='green', alpha=0.2, linewidth=0)
 
-def draw_iss(ax, longitude, latitude):
-    x, y, z = polar_to_cartesian(longitude, latitude, EARTH_RADIUS + ISS_ORBIT_RADIUS)
-    ax.scatter(x, y, z, c='blue', marker='o', label='ISS')
+def draw_points(ax, data, label):
 
-def draw_predictions(ax, preds):
-    global prediction_legend_added
+    if label == 'Pred':
+        color = 'red'
+        color_last = 'orange'
+    elif label == 'True':
+        color = 'blue'
+        color_last = 'cyan'
+    else:
+        raise ValueError(f'Pick one from ["Preds", "True"].')
 
-    for i in range(len(preds)):
-        longitude, latitude = preds[i].detach().numpy()
+    for i in range(len(data)):
+
+        longitude, latitude = data[i].detach().numpy()
         x, y, z = polar_to_cartesian(longitude, latitude, EARTH_RADIUS + ISS_ORBIT_RADIUS)
+
         if i == 0:
-            ax.scatter(x, y, z, c='orange', marker='o', label='Pred')
+            ax.scatter(x, y, z, c=color, marker='o', label=label)
+        elif i < (len(data)-1):
+            ax.scatter(x, y, z, c=color, marker='o')
         else:
-            ax.scatter(x, y, z, c='orange', marker='o')
+            ax.scatter(x, y, z, c=color_last, marker='o', label=f'Last {label}')
 
-    if not prediction_legend_added:
-        ax.legend()
-        prediction_legend_added = True
+def pause_animation(event):
+    global animation_paused
+    animation_paused = not animation_paused
 
-def update_plot(frame, model, ax, longitude, latitude, preds, r2_scores):
+def update_plot(frame, model, ax, lon_lat, preds):
     global animation_paused
     
     ax.cla()
+    draw_earth(ax)
 
     fetch_data = FetchData()
-    fetch_data = torch.tensor(list(map(float, next(fetch_data))), dtype=torch.float32)
-    net_input = fetch_data / 180
 
+    net_input_longitude = []
+    net_input_latitude = []
+    true_lon_lat = []
+
+    for i in range(DATA_STEP+1):
+
+        if i < DATA_STEP:
+            data = list(map(float, next(fetch_data)))
+            net_input_longitude.append(data[0])
+            net_input_latitude.append(data[1])
+        else:
+            lon, lat = list(map(float, next(fetch_data)))
+            true_lon_lat.append([lon, lat])
+
+    net_input = torch.tensor([net_input_longitude, net_input_latitude], dtype=torch.float32)
+    net_input = net_input.T
+    net_input = net_input.reshape(1, DATA_STEP, 2)
+
+    true_lon_lat = torch.tensor(true_lon_lat, dtype=torch.float32)
+
+    net_input = net_input / 180
     net_output = model(net_input)
     pred = net_output * 180
-    
-    lon = fetch_data[0].item()
-    lat = fetch_data[1].item()
 
-    if len(longitude) == 0 or (lon != longitude[-1] or lat != latitude[-1]):
-        longitude.append(lon)
-        latitude.append(lat)
-        preds.append(pred)
+    pred = torch.reshape(pred, (2, 1))
+    true_lon_lat = torch.reshape(true_lon_lat, (2, 1))
 
-    draw_earth(ax)
-    draw_iss(ax, longitude, latitude)
-    draw_predictions(ax, preds)
+    preds.append(pred)
+    lon_lat.append(true_lon_lat)
+
+    mae = MeanAbsoluteError()
+    mean_abs_err = mae(pred, true_lon_lat)
+
+    draw_points(ax, lon_lat, label='True')
+    draw_points(ax, preds, label='Pred')
 
     ax.set_xticks([])
     ax.set_yticks([])
@@ -82,43 +105,38 @@ def update_plot(frame, model, ax, longitude, latitude, preds, r2_scores):
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
 
+    text_true_lon = f'True Longitude: {round(lon_lat[-1][0].item(), 3)}'
+    text_true_lat = f'True Latitude: {round(lon_lat[-1][1].item(), 3)}'
+    text_pred_lon = f'Pred Longitude: {round(preds[-1][0].item(), 3)}'
+    text_pred_lat = f'Pred Latitude: {round(preds[-1][1].item(), 3)}'
+    text_mae = f'MAE: {round(mean_abs_err.item(), 3)}'
+
+    text = f'{text_true_lon}\n{text_pred_lon}\n{text_true_lat}\n{text_pred_lat}\n{text_mae}'
+    ax.text2D(0.95, 0.0, text, transform=ax.transAxes, fontsize='x-small')
+
     if animation_paused:
         ani.event_source.stop()
     else:
         ani.event_source.start()
 
-    # r2 = R2Score(num_outputs=1, multioutput='uniform_average')
-    # r2 = r2(torch.tensor(preds), torch.tensor(x_true))
-    # r2_scores.append(r2)
-
-    # # Wyświetlanie R2 Score na wykresie
-    # ax.text2D(0.05, 0.95, f'R2 Score: {r2.item():.4f}', transform=ax.transAxes)
-
     ax.legend()
 
-def pause_animation(event):
-    global animation_paused
-    animation_paused = not animation_paused
-
 def main():
-    global ani, prediction_legend_added
+    global ani
 
     torch_model = myModel()
     lit_model = myLitModel.load_from_checkpoint(
-        checkpoint_path='lightning_logs/version_0/checkpoints/epoch=23-step=2640.ckpt',
+        checkpoint_path='lightning_logs/version_0/checkpoints/epoch=16-step=1734.ckpt',
         map_location=torch.device('cpu'),
         model=torch_model)
 
-    longitude = []
-    latitude = []
+    lon_lat = []
     preds = []
-    r2_scores = []
-    prediction_legend_added = False
 
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
 
-    ani = FuncAnimation(fig, update_plot, fargs=(lit_model, ax, longitude, latitude, preds, r2_scores), frames=100, interval=500)
+    ani = FuncAnimation(fig, update_plot, fargs=(lit_model, ax, lon_lat, preds), frames=100, interval=500)
 
     fig.canvas.mpl_connect('key_press_event', pause_animation)
 
