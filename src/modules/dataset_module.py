bcf4cdec3301d50c68e7ef35eddce93cb63ac9d5
Dawidemm <dawid.mazur@icloud.com>
2024-01-15 22:18:47 +0100

    chore: remove dependency on class DataStep

--- src/modules/dataset_module.py
+++ src/modules/dataset_module.py
@@ -6,22 +6,23 @@ from lightning import LightningDataModule
 torch.manual_seed(10)
 
 class LatLongDataset(Dataset):
-    def __init__(self, csv_file: str, step: int):
+    def __init__(self, csv_file: str):
         self.data = pd.read_csv(csv_file, header=0)
         self.data = self.data.drop_duplicates()
-        self.step = step
         self.max_val = 180
         self.num_samples = len(self.data)
 
     def __len__(self):
-        return self.num_samples - self.step - 1
+        return self.num_samples - 1
 
     def __getitem__(self, index):
 
-        current_row = self.data.iloc[index:index+self.step].values
-        target = self.data.iloc[index+self.step+1].values
+        current_row = self.data.iloc[index].values
+        target = self.data.iloc[index+1].values
 
         current_row  = torch.tensor(current_row, dtype=torch.float32) /self.max_val
+        current_row = torch.reshape(current_row, (1, 2))
+
         target = torch.tensor(target, dtype=torch.float32) /self.max_val
         target = torch.reshape(target, (1, 2))
         
@@ -37,10 +38,10 @@ class LightningLatLongDatamodule(LightningDataModule):
 
     def setup(self, stage: str):
         if stage == 'fit':
-            self.train_dataset = LatLongDataset(csv_file=self.train_csv, step=1)
-            self.val_dataset = LatLongDataset(csv_file=self.val_csv, step=1)
+            self.train_dataset = LatLongDataset(csv_file=self.train_csv)
+            self.val_dataset = LatLongDataset(csv_file=self.val_csv)
         if stage == 'test':
-            self.test_dataset = LatLongDataset(csv_file=self.test_csv, step=1)
+            self.test_dataset = LatLongDataset(csv_file=self.test_csv)
 
     def train_dataloader(self) -> DataLoader:
         return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=False)
